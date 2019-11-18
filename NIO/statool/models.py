9802c6791bc4d5cde384796fda38f512f48f4867
calvarez <38008807+arkadin-calvarez@users.noreply.github.com>
2019-11-18 16:50:19 -0300

    fixing typos

--- NIO/statool/models.py
+++ NIO/statool/models.py
@@ -5,10 +5,10 @@ from django.db import models
 
 # Create your models here.
 class Host(models.Model):
-    name = models.CharField(max_lenght=100)
-    name = models.CharField(max_lenght=70)
-    username = models.CharField(max_lenght=100)
-    password = models.CharField(max_lenght=70)
+    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=70)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=70)
     service_type = models.Charfield(
-        max_lenght=30, choices=(("AM", "CCA AMERICAS"), ("AP", "CCA APAC"), ("EM", "CCA EMEA")
+        max_length=30, choices=(("AM", "CCA AMERICAS"), ("AP", "CCA APAC"), ("EM", "CCA EMEA")
     )
