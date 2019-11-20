853034f944baa52b5ad96f361f3f4c48b9907eef
calvarez <38008807+arkadin-calvarez@users.noreply.github.com>
2019-11-20 17:49:43 -0300

    adding device section to views

--- NIO/statool/views.py
+++ NIO/statool/views.py
@@ -2,13 +2,16 @@
 #from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
+from .models import Device
 
 # Create your views here.
 
 def index(request: HttpRequest) -> HttpResponse:
+    devices = Device.objects.all()
     context = {
         'title' : 'Hello Personas',
-        'nickname' : 'Dear OutRight'
+        'nickname' : 'Dear OutRight',
+        'devices' : devices
     }
     return render(request, 'base.html', context)
 
