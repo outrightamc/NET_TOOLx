f64c53edbc86edfb235aa8dd7cca4d870b521cc9
calvarez <38008807+arkadin-calvarez@users.noreply.github.com>
2019-11-20 18:18:05 -0300

    adding host, platf, device_type to models.py

--- NIO/statool/models.py
+++ NIO/statool/models.py
@@ -6,12 +6,16 @@ from django.db import models
 # Create your models here.
 class Device(models.Model):
     name = models.CharField(max_length=100)
-    name = models.CharField(max_length=70)
+    host = models.CharField(max_length=70)
     username = models.CharField(max_length=100)
     password = models.CharField(max_length=70)
-    service_type = models.CharField(
+    device_type = models.CharField(
         max_length=30, 
-        choices=(('router1', 'Router'), ('router2', 'Router+'), ('router2', 'Router++'))
+        choices=(('router', 'Router'), ('switch', 'Switch'), ('firewall', 'Firewall'))
+    )
+    platform = models.CharField(
+        max_length=30, 
+        choices=(('cisco', 'Cisco IOS'), ('junos', 'JUNOS'))
     )
 
     def __str__(self) -> str:
