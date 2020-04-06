e7e871da2e43555aa18de622bff73819e3a537bf
OutRight <outright@gmail.com>
2020-04-05 23:19:24 -0300

    ok

--- NIO/NIO/urls.py
+++ NIO/NIO/urls.py
@@ -22,9 +22,9 @@ from accounts.views import login_view, register_view
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    path('home/', home),
+    path('home.html', home),
     path('', include ('statool.urls')),
     path('accounts/login/', login_view),
     path('accounts/register/', register_view),
-
+    path('accounts/logout/', logout_view),
 ]
